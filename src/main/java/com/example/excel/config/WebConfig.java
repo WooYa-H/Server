73aca2de6deedf2ad089fae571c72e8671a8bda4
RapiL <sjw3090gmail.com>
2024-05-22 22:10:06 +0900

    cors 추가

---
+++ src/main/java/com/example/excel/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.excel.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class WebConfig {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name());
+    }
+}
