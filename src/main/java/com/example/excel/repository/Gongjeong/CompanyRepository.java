439e08c2d38d43fb439d85a51797c982f5f973ba
RapiL <sjw3090@gmail.com>
2024-05-25 12:44:38 +0900

    회사명 중복 체크 로적 작성

--- src/main/java/com/example/excel/repository/Gongjeong/CompanyRepository.java
+++ src/main/java/com/example/excel/repository/Gongjeong/CompanyRepository.java
@@ -4,7 +4,10 @@ import com.example.excel.domain.Gongjeong.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
+    boolean existsByName(String name);
 }
