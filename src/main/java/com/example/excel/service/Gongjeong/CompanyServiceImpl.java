f91ae88795150204cf5d955b971580144bb22ca9
RapiL <sjw3090@gmail.com>
2024-05-25 12:54:07 +0900

    회사 생성 코드 추가

--- src/main/java/com/example/excel/service/Gongjeong/CompanyServiceImpl.java
+++ src/main/java/com/example/excel/service/Gongjeong/CompanyServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.excel.service.Gongjeong;
 
+import com.example.excel.domain.Gongjeong.Company;
+import com.example.excel.dto.Gongjeong.CreateCompanyDTO;
 import com.example.excel.repository.Gongjeong.CompanyRepository;
+import com.example.exception.Gonjeong.CompanyException;
+import com.example.type.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,4 +15,21 @@ public class CompanyServiceImpl implements CompanyService{
     private final CompanyRepository companyRepository;
 
 
+    private Company createCompany(CreateCompanyDTO.Request request) {
+
+        if (companyRepository.existsByName(request.getName())) {
+            throw new CompanyException(ErrorCode.COMPANY_NAME_DUPLICATED);
+        }
+
+        Company company = Company.builder()
+                .ceo(request.getCeo())
+                .email(request.getEmail())
+                .address(request.getAddress())
+                .name(request.getName())
+                .phoneNumber(request.getPhoneNumber())
+                .businessNumber(request.getBusinessNumber())
+                .build();
+
+        return companyRepository.save(company);
+    }
 }
