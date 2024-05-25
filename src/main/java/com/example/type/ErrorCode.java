0ddbb99a746852916e94d48c5fcdfedfc2859d4e
RapiL <sjw3090@gmail.com>
2024-05-25 13:01:16 +0900

    회사 찾을수 없습니다 추가

--- src/main/java/com/example/type/ErrorCode.java
+++ src/main/java/com/example/type/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    COMPANY_NAME_DUPLICATED("회사명이 이미 존재합니다");
+    COMPANY_NAME_DUPLICATED("회사명이 이미 존재합니다"),
+    COMPANY_NOT_FOUND("회사를 찾을 수 없습니다.");
 
     private final String Description;
 }
