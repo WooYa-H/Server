12013e2a6ca1fa93336cb4a7b6cc10cf034d18da
RapiL <sjw3090@gmail.com>
2024-05-25 12:53:49 +0900

    회사 이름 중복 익셉션 코드 추가

--- src/main/java/com/example/type/ErrorCode.java
+++ src/main/java/com/example/type/ErrorCode.java
@@ -6,5 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    COMPANY_NAME_DUPLICATED()
+    COMPANY_NAME_DUPLICATED("회사명이 이미 존재합니다");
+
+    private final String Description;
 }
