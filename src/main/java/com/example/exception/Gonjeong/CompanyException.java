a2123cfb28b411773e36cab0ddf6ac201e6ac46b
RapiL <sjw3090@gmail.com>
2024-05-25 12:43:45 +0900

    익셉션 추가

---
+++ src/main/java/com/example/exception/Gonjeong/CompanyException.java
@@ -0,0 +1,17 @@
+package com.example.exception.Gonjeong;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CompanyException extends RuntimeException {
+
+    private ErrorC;
+
+    public CompanyException(ErrorCode
+                            )
+}
---
+++ src/main/java/com/example/type/ErrorCode.java
@@ -0,0 +1,10 @@
+package com.example.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    COMPANY_NAME_DUPLICATED()
+}
