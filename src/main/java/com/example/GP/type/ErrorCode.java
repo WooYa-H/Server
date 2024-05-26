472b71171d483b33e34640215882d22c8993abc3
RapiL <sjw3090@gmail.com>
2024-05-26 11:06:11 +0900

    부서 익셉션 추가

--- src/main/java/com/example/GP/type/ErrorCode.java
+++ src/main/java/com/example/GP/type/ErrorCode.java
@@ -7,7 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     COMPANY_NAME_DUPLICATED("회사명이 이미 존재합니다"),
-    COMPANY_NOT_FOUND("회사를 찾을 수 없습니다.");
+    COMPANY_NOT_FOUND("회사를 찾을 수 없습니다."),
+    DEPARTMENT_NOT_FOUND("부서를 찾을 수 없습니다."),
+    DEPARTMENT_NAME_DUPLICATED("부서명이 이미 존재합니다");
 
     private final String Description;
 }
