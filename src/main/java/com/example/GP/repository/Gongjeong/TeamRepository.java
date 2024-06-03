d5f82201267cb92e25e7880750c73045396b8fdd
RapiL <sjw3090@gmail.com>
2024-06-03 22:34:00 +0900

    부서에서 팀 조회를 위한 리스트 추가

--- src/main/java/com/example/GP/repository/Gongjeong/TeamRepository.java
+++ src/main/java/com/example/GP/repository/Gongjeong/TeamRepository.java
@@ -4,8 +4,12 @@ import com.example.GP.domain.Gongjeong.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
 
     boolean existsByTeamName(String name);
+
+    List<Team> findByBusinessId(Long businessId);
 }
