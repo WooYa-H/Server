018e3813696a0829426ea272d4cdcf45d95b3cd2
rapil <sjw3090@gmail.com>
2024-06-04 16:56:35 +0900

    팀원 조회시 사번도 같이 조회되도록 수정

--- src/main/java/com/example/GP/dto/Gongjeong/TeamMemberDTO.java
+++ src/main/java/com/example/GP/dto/Gongjeong/TeamMemberDTO.java
@@ -13,9 +13,12 @@ public class TeamMemberDTO {
     private Long id;
     
     private String teamRank;
+
+    private String employeeNumber;
     
     public TeamMemberDTO(TeamMember teamMember) {
         this.id = teamMember.getId();
         this.teamRank = teamMember.getTeamRank();
+        this.employeeNumber = teamMember.getEmployeeNumber();
     }
 }
