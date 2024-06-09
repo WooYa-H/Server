aa036e6b71871fc556e5f069add0841d1c4808f6
RapiL <sjw3090@gmail.com>
2024-06-09 12:02:37 +0900

    사업에 소속된 팀 조회 로직 추가

--- src/main/java/com/example/GP/repository/Gongjeong/TeamRepository.java
+++ src/main/java/com/example/GP/repository/Gongjeong/TeamRepository.java
@@ -2,6 +2,8 @@ package com.example.GP.repository.Gongjeong;
 
 import com.example.GP.domain.Gongjeong.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
     boolean existsByTeamName(String name);
 
     List<Team> findByBusinessId(Long businessId);
+
+    @Query("SELECT t FROM Team t WHERE t.business.id IN :businessIds")
+    List<Team> findAllByBusinessIdIn(@Param("businessIds") List<Long> businessIds);
 }
