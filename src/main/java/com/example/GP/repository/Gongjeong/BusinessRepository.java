2a157d624d2157e89d9c93728635a6b422d3677c
RapiL <sjw3090@gmail.com>
2024-06-09 12:01:53 +0900

    부서에 소속된 사업 조회 로직 추가

--- src/main/java/com/example/GP/repository/Gongjeong/BusinessRepository.java
+++ src/main/java/com/example/GP/repository/Gongjeong/BusinessRepository.java
@@ -1,14 +1,21 @@
 package com.example.GP.repository.Gongjeong;
 
 import com.example.GP.domain.Gongjeong.Business;
+import com.example.GP.domain.Gongjeong.Department;
 import org.apache.poi.ss.formula.functions.T;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BusinessRepository extends JpaRepository<Business, Long> {
 
     boolean existsByBusinessName(String name);
+
+    @Query("SELECT b FROM Business b WHERE b.department.id IN :departmentIds")
+    List<Business> findAllByDepartmentIdIn(@Param("departmentIds") List<Long> departmentIds);
 }
